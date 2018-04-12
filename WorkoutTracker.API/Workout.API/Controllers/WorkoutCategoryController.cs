c3e038eca34e792bfc0de30b4b9f07367ea908f9
Nakash Sheikh <nakash2050@gmail.com>
2018-04-12 13:23:57 +0530

    Added WebApi Project

---
+++ WorkoutTracker.API/Workout.API/Controllers/WorkoutCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using WorkoutTracker.BAL;
+using WorkoutTracker.Entities;
+
+namespace WorkoutTracker.API.Controllers
+{
+    [RoutePrefix("api/category/")]
+    public class WorkoutCategoryController : ApiController
+    {
+        private readonly WorkoutCategoryBAL _categoryBAL;
+
+        public WorkoutCategoryController()
+        {
+            _categoryBAL = new WorkoutCategoryBAL();
+        }
+
+        public IHttpActionResult PostCategory(WorkoutCategory category)
+        {
+            var result = _categoryBAL.AddWorkoutCategory(category);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllCategories()
+        {
+            var result = _categoryBAL.GetAllWorkoutCategories();
+            return Ok(result);
+        }
+    }
+}
---
+++ WorkoutTracker.BAL/WorkoutCategoryBAL.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using WorkoutTracker.DAL;
+using WorkoutTracker.DAL.Repositories;
+using WorkoutTracker.Entities;
+
+namespace WorkoutTracker.BAL
+{
+    public class WorkoutCategoryBAL
+    {
+        public bool AddWorkoutCategory(WorkoutCategory category)
+        {
+            using (var unitOfWork = new UnitOfWork(new WorkoutTrackerContext()))
+            {
+                unitOfWork.WorkoutCategory.Add(category);
+                var result = unitOfWork.Complete();
+                return result == 1;
+            }
+        }
+
+        public WorkoutCategory GetWorkoutCategory(int id)
+        {
+            using (var unitOfWork = new UnitOfWork(new WorkoutTrackerContext()))
+            {
+                var result = unitOfWork.WorkoutCategory.Get(id);
+                return result;
+            }
+        }
+
+        public IEnumerable<WorkoutCategory> GetAllWorkoutCategories()
+        {
+            using (var unitOfWork = new UnitOfWork(new WorkoutTrackerContext()))
+            {
+                var result = unitOfWork.WorkoutCategory.GetAll();
+                return result;
+            }
+        }
+    }
+}
