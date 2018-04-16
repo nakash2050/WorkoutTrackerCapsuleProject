803af3cb416e439c2069113eaa9453cb4fa202ed
nakash2050 <10336949+nakash2050@users.noreply.github.com>
2018-04-16 20:12:55 +0530

    Updated safety check

--- WorkoutTracker.SPA/src/app/track/track.component.ts
+++ WorkoutTracker.SPA/src/app/track/track.component.ts
@@ -14,9 +14,9 @@ export class TrackComponent implements OnInit {
   totalWorkoutTime: number = 0;
 
   constructor(
-    private router: Router, 
+    private router: Router,
     private route: ActivatedRoute
-  ) {}
+  ) { }
 
   public barChartOptions: any = {
     scaleShowVerticalLines: true,
@@ -68,7 +68,7 @@ export class TrackComponent implements OnInit {
       label: ''
     }
   ];
-  
+
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.report = data["report"];
@@ -76,33 +76,39 @@ export class TrackComponent implements OnInit {
       //Weekly Calories Report
       const totalCaloriesBurntPerWeek = this.report.totalCaloriesBurntPerWeek;
       let totalWeeklyCalories = 0;
-      for (let data of totalCaloriesBurntPerWeek) {
-        this.barChartWeekLabels.push(data.duration);
-        this.barChartWeekData[0].data.push(data.totalCaloriesBurnt);
-        totalWeeklyCalories = totalWeeklyCalories + +data["totalCaloriesBurnt"];
-      }     
-      this.barChartWeekData[0].label = "Weekly Calories Burnt: " + totalWeeklyCalories;
+      if (totalCaloriesBurntPerWeek) {
+        for (let data of totalCaloriesBurntPerWeek) {
+          this.barChartWeekLabels.push(data.duration);
+          this.barChartWeekData[0].data.push(data.totalCaloriesBurnt);
+          totalWeeklyCalories = totalWeeklyCalories + +data["totalCaloriesBurnt"];
+        }
+        this.barChartWeekData[0].label = "Weekly Calories Burnt: " + totalWeeklyCalories;
+      }
 
       //Monthly Calories Report
       const totalCaloriesBurntPerMonth = this.report.totalCaloriesBurntPerMonth;
       let totalMonthlyCalories = 0;
-      for (let data of totalCaloriesBurntPerMonth) {
-        this.barChartMonthLabels.push(data.duration);
-        this.barChartMonthData[0].data.push(data.totalCaloriesBurnt);
-        totalMonthlyCalories = totalMonthlyCalories + +data["totalCaloriesBurnt"];
-      }     
-      this.barChartMonthData[0].label = "Monthly Calories Burnt: " + totalMonthlyCalories;
+      if (totalCaloriesBurntPerMonth) {
+        for (let data of totalCaloriesBurntPerMonth) {
+          this.barChartMonthLabels.push(data.duration);
+          this.barChartMonthData[0].data.push(data.totalCaloriesBurnt);
+          totalMonthlyCalories = totalMonthlyCalories + +data["totalCaloriesBurnt"];
+        }
+        this.barChartMonthData[0].label = "Monthly Calories Burnt: " + totalMonthlyCalories;
+      }
 
-         //Monthly Calories Report
+      //Monthly Calories Report
       const totalCaloriesBurntPerYear = this.report.totalCaloriesBurntPerYear;
       let totalYearlyCalories = 0;
-      for (let data of totalCaloriesBurntPerYear) {
-        this.barChartYearLabels.push(data.duration);
-        this.barChartYearData[0].data.push(data.totalCaloriesBurnt);
-        totalYearlyCalories = totalYearlyCalories + +data["totalCaloriesBurnt"];
-      }      
-      this.barChartYearData[0].label = "Yearly Calories Burnt: " + totalYearlyCalories;
-    
+      if (totalCaloriesBurntPerYear) {
+        for (let data of totalCaloriesBurntPerYear) {
+          this.barChartYearLabels.push(data.duration);
+          this.barChartYearData[0].data.push(data.totalCaloriesBurnt);
+          totalYearlyCalories = totalYearlyCalories + +data["totalCaloriesBurnt"];
+        }
+        this.barChartYearData[0].label = "Yearly Calories Burnt: " + totalYearlyCalories;
+      }
+
     });
   }
 }
